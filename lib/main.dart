d198a68f6133866187da2e0922245fd1484e7aa7
littleironical <hardik.kumar18feb@gmail.com>
2023-08-02 18:56:47 +0530

    Disables landscape mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,11 @@
 import 'package:culture_circle_app_task/app/app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+      
+  runApp(const MyApp());
+}
